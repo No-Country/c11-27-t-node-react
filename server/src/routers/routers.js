974f1044d9c569e5d6bc0f6599020f6eb88720e8
cristian16260 <124100432+cristian16260@users.noreply.github.com>
2023-05-23 16:37:11 -0500

    fix: updated endpoint

--- server/src/routers/routers.js
+++ server/src/routers/routers.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const {register, listusuarios, login, actualizarcontrasena, actualizardatos} = require('../controllers/usuarioscontroller')
+const {register, listusuarios, login, actualizarcontrasena, actualizardatos, protect} = require('../controllers/usuarioscontroller')
 const {generateBusqueda} = require('../controllers/openAI')
 
 const routeruser = express.Router()
 
 routeruser.post('/', register)
-routeruser.get('/', listusuarios)
 routeruser.post('/login', login)
-routeruser.post('/reset', actualizarcontrasena)
+// routeruser.use(protect) proteccion de token
+routeruser.put('/reset', actualizarcontrasena)
 routeruser.post('/search', generateBusqueda)
-routeruser.post('/renew', actualizardatos)
-
+routeruser.put('/renew', actualizardatos)
+routeruser.get('/', listusuarios)
 module.exports = routeruser
