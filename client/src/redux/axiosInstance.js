518b9d68a69ca7c7608e8691652fc4de935a0d2a
Aeternus185 <andryp_@hotmail.com>
2023-06-02 19:17:53 -0400

    fix: 🐛 replace dotenv for vite env

--- client/src/redux/axiosInstance.js
+++ client/src/redux/axiosInstance.js
@@ -1,12 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config({
-  path: "../../.env",
-});
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axiosInstance;
