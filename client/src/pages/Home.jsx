3372aa6c8c8500ba4d557c6193ed542ff1b260a0
Aeternus185 <andryp_@hotmail.com>
2023-06-03 15:47:28 -0400

    chore: ⚰️ remove code for dev test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,6 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 const Home = () => {
-  useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_API_URL}/api/hello`)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error("Error al hacer la solicitud:", error);
-      });
-  }, []);
   return <div className="text-2xl font-bold">Home</div>;
 };
 
