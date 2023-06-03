752645b422a3619a6e3a6d11a559b198863a241e
Aeternus185 <andryp_@hotmail.com>
2023-06-03 14:35:15 -0400

    refactor: ⚰️ remove unnecessary import

--- client/src/components/Ui/DayCard.jsx
+++ client/src/components/Ui/DayCard.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Button from "./Button";
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCalendarDay,
