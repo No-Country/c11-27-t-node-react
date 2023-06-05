c4928e06c4a9a98b197dd087a418393b2b755806
Andry Orellana <44151183+AndryOre@users.noreply.github.com>
2023-06-05 04:53:11 -0400

    fix: 🐛 increase openai max tokens

--- server/src/controllers/openAI.js
+++ server/src/controllers/openAI.js
@@ -1,22 +1,22 @@
-const {Configuration,OpenAIApi} = require('openai')
-const {itinerarios} = require('../models/itinrario')
-const dotenv = require('dotenv')
+const { Configuration, OpenAIApi } = require('openai');
+const { itinerarios } = require('../models/itinrario');
+const dotenv = require('dotenv');
 
 dotenv.config({
-  path:'../.env'
-})
+	path: '../.env',
+});
 
 const configuration = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY || ''
+	apiKey: process.env.OPENAI_API_KEY || '',
 });
 
 const openAi = new OpenAIApi(configuration);
 
-const generateBusqueda = async (req,res) => {
-    const {_id, destino, fechainicio, fechafinal, intereses} = req.body
-    const result = await openAi.createCompletion({
-        model: "text-davinci-003",
-        prompt:`Estoy planeando un viaje a ${destino} desde el ${fechainicio} hasta el ${fechafinal}. Estoy especialmente interesado en ${intereses}. Me gustaría que me generes un itinerario en formato JSON que incluya:
+const generateBusqueda = async (req, res) => {
+	const { _id, destino, fechainicio, fechafinal, intereses } = req.body;
+	const result = await openAi.createCompletion({
+		model: 'text-davinci-003',
+		prompt: `Estoy planeando un viaje a ${destino} desde el ${fechainicio} hasta el ${fechafinal}. Estoy especialmente interesado en ${intereses}. Me gustaría que me generes un itinerario en formato JSON que incluya:
         Un número de día, un título temático para cada día basado en las actividades planeadas, y una lista de actividades. Cada actividad debe incluir un lugar, google maps url del lugar, una breve descripción y la razón por la cual se recomienda.
         Un ejemplo de la estructura deseada sería:
         {
@@ -38,23 +38,23 @@ const generateBusqueda = async (req,res) => {
            ]
            }
            `,
-        max_tokens: 250,
-        temperature: 0.2,
-    })  
-    const busqueda = result.data.choices[0].text;
-    const itinerario = new itinerarios({
-        id_user: _id,
-        search: busqueda
-    })
+		max_tokens: 4097,
+		temperature: 0.2,
+	});
+	const busqueda = result.data.choices[0].text;
+	const itinerario = new itinerarios({
+		id_user: _id,
+		search: busqueda,
+	});
 
-    await itinerario.save();
+	await itinerario.save();
 
-    res.status(200).json({
-        success: true,
-        data: busqueda
-    })
-}
+	res.status(200).json({
+		success: true,
+		data: busqueda,
+	});
+};
 
 module.exports = {
-    generateBusqueda
-}
+	generateBusqueda,
+};
