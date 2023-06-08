fa93b42f862389dc2f1191a5b20e66f847733dc4
Aeternus185 <andryp_@hotmail.com>
2023-06-08 10:36:31 -0400

    feat: ✨ add not found page

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "../components/Ui/Header";
+import Footer from "../components/Ui/Footer";
+import PageNotFoundImage from "../assets/page-not-found.svg";
 
 const NotFound = () => {
-  return <div className="text-2xl font-bold">Not Found</div>;
+  return (
+    <div className="flex h-screen flex-col justify-between bg-neutral-50">
+      <Header pageType="auth" />
+      <div className="flex flex-col items-center justify-center p-4">
+        <img
+          src={PageNotFoundImage}
+          alt="Reset Password Image"
+          className="h-max"
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default NotFound;
